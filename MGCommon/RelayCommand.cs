21ed125974bc3dc45f359cf1f61327aea45615d9
Matthew <Matthew@STUDY-XPS>
2016-11-17 11:58:11 +1100

    Add project files.

---
+++ MGCommon/RelayCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MGCommon
+{
+    public class RelayCommand : ICommand
+    {
+        Action<object> execute;
+        Predicate<object> canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> execute)
+            :this(execute, null)
+        { }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute != null && canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
---
+++ MGCommon/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+
+namespace MGCommon
+{
+    /// <summary>
+    /// Extenstion Utils
+    /// </summary>
+    public static class Utils
+    {
+        /// <summary>
+        /// Task extenstion to add a timeout.
+        /// </summary>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        /// <param name="task">Task.</param>
+        /// <param name="duration">Duration</param>
+        /// <returns>The timeout</returns>
+        public async static Task<T> WithTimeout<T>(this Task<T> task, int duration)
+        {
+            var retTask = await Task.WhenAny(task, Task.Delay(duration)).ConfigureAwait(false);
+
+            if (retTask is Task<T>)
+                return task.Result;
+
+            return default(T);
+        }
+    }
+}
