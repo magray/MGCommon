b05ea96e698928d89d5484e7d3dbe382057d566d
Matthew Gray <magray@live.com>
2019-02-03 12:30:41 +1100

    Added serializer and xml abstract class

---
+++ MGCommon.net45/Xml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace MGCommon
+{
+    public abstract class XmlFile
+    {
+        private XmlDocument xml_doc;
+
+        protected IXPathNavigable Doc
+        {
+            get
+            {
+                return xml_doc;
+            }
+        }
+
+        public string FilePath { get; }
+
+        protected XmlFile(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentException("filePath");
+            }
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("File not found", filePath);
+            }
+            FilePath = filePath;
+            xml_doc = new XmlDocument();
+            xml_doc.Load(FilePath);
+        }
+    }
+}
